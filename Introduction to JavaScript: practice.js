7f2c4ac6db67ca1549d06a553661b405dfc8da70
Simone van der Graaf <108785676+SimonevanderGraaf@users.noreply.github.com>
2025-01-23 14:29:28 +0100

    Create Introduction to JavaScript: practice.js

---
+++ Introduction to JavaScript: practice.js
@@ -0,0 +1,43 @@
+/* <b>Topic 1: Introduction to JavaScript</b>
+
+Part 1: change the dots by filling in the missing code
+
+Example:
+... name = 'User';
+let age = 20;
+let welcomeMessage = 'Hello, ' + name + '. You are ' + age + ' years old.';
+... (welcomeMessage);
+
+Result:*/
+let name = 'User';
+let age = 20;
+let welcomeMessage = 'Hello, ' + name + '. You are ' + age + ' years old.';
+console.log (welcomeMessage);
+
+/* Example:
+... myName = "John Doe";
+var introduction = "Hello, My name is ";
+... (introduction + myName);
+
+Result: */
+var myName = "John Doe";
+var introduction = "Hello, My name is ";
+console.log (introduction + myName);
+
+/* Example:
+function (...) {
+... ("Greetings, realm of JavaScript!");
+}
+shoutToTheWorld...;
+
+Result: */
+function (shoutToTheWorld) {
+console.log ("Greetings, realm of JavaScript!");
+}
+shoutToTheWorld();
+
+/* Part 2: Display the I like JavaScript! phrase using console.log based on the code from the theory.
+Answer: */
+console.log ("I like JavaScript!");
+
+/* <b>Topic 2: Writing first program</b>
