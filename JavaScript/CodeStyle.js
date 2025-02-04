ec6b034ccd60abda31f663dd5d7c1ea824d42215
Simone van der Graaf <s.vandergraaf@yahoo.com>
2025-02-04 19:06:45 +0100

    Updated and added files

---
+++ JavaScript/CodeStyle.js
@@ -0,0 +1,94 @@
+/**
+ * Topic 1: Comments
+ */
+
+/*
+Question 1:
+Comment unnecessary lines of code to make sure that the result of the output is as follows:
+A cat
+says
+meow
+*/
+
+console.log("A dog");
+console.log("A cat");
+console.log("says");
+console.log("quack-quack");
+console.log("woof");
+console.log("meow");
+
+// Answer:
+// console.log("A dog");
+console.log("A cat");
+console.log("says");
+/* console.log("quack-quack");
+console.log("woof"); */
+console.log("meow");
+
+/**
+ * Topic 2: Naming Variables
+ */
+
+/*
+Question 1:
+Select all valid variable names.
+ */
+
+myName
+1day
+error_message
+red-color
+
+// Answer: myName and error_message are correct.
+
+/*
+Question 2:
+Remove all invalid characters from $-disabled to make it a valid name for a variable. Enter the result.
+ */
+
+// Answer:
+$disabled
+
+/*
+Question 3:
+A student created a name 12-key Code for a variable, but forgot about the camelCase naming convention.
+Remove all invalid characters from the name to make it a valid one. Enter the result.
+ */
+
+// Answer:
+keyCode
+
+/*
+Question 4:
+The JavaScript naming conventions specify groups of characters that are allowed at the beginning of a variable name.
+Which characters cannot be used to start a name?
+ */
+
+// Answer: digits
+
+/*
+Question 5:
+Which of the following words cannot be used to name a variable since they are reserved?
+ */
+
+let
+age
+function
+day
+
+// Answer: let and function
+
+/*
+Question 6:
+Here's the list of variables:
+*/
+
+let p = 10;
+let login = "username";
+let clientName = "Bob";
+let coloroftheshirt = "black";
+
+// Some are named according to the naming conventions and best practices, while others are not.
+// Select only those variables that have good names.
+
+// Answer: login and clientName
